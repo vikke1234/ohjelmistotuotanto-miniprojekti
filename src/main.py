8f3275f5ede9c73da3d6e59ac0a359a7e76b5c84
Viktor Horsmanheimo <viktor.horsmanheimo@helsinki.fi>
2021-11-23 13:33:06 +0200

    added hello world tui

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from ui.tui import Tui
 
 def main(stdscr):
     tui = Tui(stdscr)
+    tui.hello_world()
 
 if __name__ == "__main__":
     wrapper(main)
--- src/ui/tui.py
+++ src/ui/tui.py
@@ -1,15 +1,20 @@
 import curses
 
 class Tui:
+    """
+    Class to show TUI
+    """
+
     def __init__(self, stdscr) -> None:
+        """
+        :stdscr: screen object from curses.initscr()
+        """
         self.stdscr = stdscr
         curses.noecho()
         curses.cbreak()
         self.stdscr.keypad(True)
+
+    def hello_world(self):
         self.stdscr.addstr(0, 0, "Hello world")
         self.stdscr.refresh()
         self.stdscr.getkey()
-
-    def destroy(self):
-        curses.endwin()
-
