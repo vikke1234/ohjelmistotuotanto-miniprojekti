440536a31a315074174d5f2670f94deeb86fd78c
Viktor Horsmanheimo <viktor.horsmanheimo@gmail.com>
2021-11-28 17:59:22 +0200

    fix pylint errors and also now clears text from lineedi

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,17 +1,23 @@
-from PyQt5.Qt import QMainWindow
-from PyQt5.QtWidgets import QListWidgetItem
+from PyQt5.QtWidgets import QListWidgetItem, QMainWindow
 
 from gui.components.mainwindow import Ui_MainWindow
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    """
+    Main window class
+    """
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
         self.add_button.clicked.connect(self.add_to_list)
 
     def add_to_list(self):
+        """
+        Adds the content of the url line edit to the saved tips list
+        """
         text = self.url_lineedit.text()
         item = QListWidgetItem()
         item.setText(text)
         current = self.listWidget.count()
         self.listWidget.insertItem(current+1, item)
+        self.url_lineedit.setText("")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import sys
 
-from PyQt5.Qt import QApplication
+from PyQt5.QtWidgets import QApplication
 
 from gui.main_window import MainWindow
 
 
 def main():
+    """
+    Main function to start application
+    """
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
     mainwindow.show()
